08efa260f681d1cb73f0267f90c5e504d8cc2396
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-02-16 11:49:54 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char : %d", sizeof(char));
+	printf("size of int : %d", sizeof(int));
+	printf("size of long int : %d", sizeof(long int));
+	printf("size of long long int %d", sizeof(long long int));
+	printf("size of float %d", sizeof(float));
+	return (0);
+}
