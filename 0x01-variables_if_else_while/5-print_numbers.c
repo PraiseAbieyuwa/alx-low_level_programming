050d196fce89e4c51eddefe13da7f92c84bec014
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-02-17 17:11:17 -0800

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - print single numbers in base 10
+* Return: Return always (0)
+*/
+
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i <= 10 ; i++)
+		putchar (i);
+	putchar('\n');
+	return (0);
+}
