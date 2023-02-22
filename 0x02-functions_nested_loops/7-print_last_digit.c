9d03a09c16e0f8d0bfaedef508a53c464a352ba6
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-02-21 16:54:01 -0800

    lasr digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that print last digit of a number
+ *
+ * @a: function of the last digit
+ * Return: Return i
+ */
+
+int print_last_digit(int a)
+{
+	int i;
+
+	i = a % 10;
+	if (a < 0)
+		i = -(i);
+	return (i);
+}
