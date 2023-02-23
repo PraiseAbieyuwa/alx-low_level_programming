c2824a2ef6140ca51f2f2e047410e6f3379f7672
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-02-23 15:00:06 -0800

    print digit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers from 0 to 9
+ *
+ * @i: char 
+ * Return: Return 0 and 1
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		_putchar('i');
+
+	_putchar('\n');
+}
