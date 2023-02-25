ad70be7557fed334cd579ae577bf64d80a3005d1
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-02-24 17:16:18 -0800

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,6 +2,7 @@
 
 /**
  * print_triangle - print traingle with #'s with given size
+ *
  * @size: size of triangle to draw
  */
 
@@ -10,18 +11,18 @@ void print_triangle(int size)
 	int height;
 	int width;
 	int draw;
-	
+
 	if (size <= 0)
 		_putchar('\n');
-	
+
 	for (height = 1; height <= size; height++)
 	{
 		for (width = 1; width <= (size - height); width++)
 			_putchar(' ');
-		
+
 		for (draw = 1; draw <= height; draw++)
 			_putchar('#');
-		
+
 		_putchar('\n');
 	}
 }
