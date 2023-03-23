7cdc5b7bcb25249553c59937c44ec699e81c6b12
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-03-22 19:35:34 -0700

    What's my name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,12 @@
+#ifndef FUNCTION_POINTERS
+#define FUNCTION_POINTERS
+
 #include <stddef.h>
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
+
 #endif
