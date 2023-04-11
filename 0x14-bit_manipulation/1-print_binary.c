f11aefaf856804155f2286f473d0237548d7a5df
PraiseAbieyuwa <alohanpraise2002@gmail.com>
2023-04-10 17:05:34 -0700

    git binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_binary - function that prints the binary of an integer
+ * @n: number of elemnts in a function
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+		_putchar('0');
+}			
