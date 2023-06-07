9e213aef76df19f9647a325970a66dad616c2e19
root <alohanpraise2002@gmail.com>
2023-06-07 08:49:11 -0700

    C- Recursion

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,8 +1,8 @@
 #ifndef RECURSION
 #define RECURSION
 
-#include <stdio.h>;
-#include <string.h>;
+#include <stdio.h>
+#include <string.h>
 
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
