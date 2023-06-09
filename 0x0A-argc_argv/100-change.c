67d73ef3c9e30b3e7f55b70fa9c3a864bf025d66
root <alohanpraise2002@gmail.com>
2023-06-09 08:05:44 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- *
+ * main - function that prints the minimum number of coins a given amount.
+ * @argc: command line arguments.
+ * @argv: array that contains strings of a command line arguments.
+ * Return: 0 - success.
  */
+int main(int argc, char *argv[])
+{
+	int cash, rem = 0;
+	/*declare variables*/
 
+	if (argc != 2)/*checks if argc has more than 1 character*/
+	{
+		printf("Error\n");
+		return (1);
+	}
+	/*parse the parameter to argument*/
+	cash = atoi(argv[1]);
+
+	while (cash > 0)
+	{
+		if (cash  >= 25)
+			cash  -= 25;
+		else if (cash  >= 10)
+			cash  -= 10;
+		else if (cash  >= 5)
+			cash  -= 5;
+		else if (cash  >= 2)
+			cash  -= 2;
+		else if (cash  >= 1)
+			cash  -= 1;
+		rem += 1;
+	}
+	printf("%d\n", rem);
+	return (0);
+}
