b17f9880c197a4461edcc372e17efa305678e33f
root <alohanpraise2002@gmail.com>
2023-06-09 05:37:52 -0700

    prints out numbers of argument passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 /**
- *
- *
+ * main - function that print the numbers of argument passed
+ * @argc: argument command line
+ * @argv: string of array thats stores the argumnet count
+ * Return: 0 for success
  */
-
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+	return (0);
+}
