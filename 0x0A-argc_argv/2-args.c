86c4d0f47003495f7c0038b10a1755d2b41de251
root <alohanpraise2002@gmail.com>
2023-06-09 06:52:02 -0700

    display arguments given

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 /**
- *
+ * main - function that prints out arguments passed to it
+ * @argc: argument command line
+ * @argv: argument vector w=that stores array of strings command line
+ * Return: 0 if successful
  */
+int main(int argc, char *argv[])
+{
+	int ast;
 
+	for (ast = 0; ast < argc; ast++)
+		printf("%s\n", argv[ast]);
+	return (0);
+}
