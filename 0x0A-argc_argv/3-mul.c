097cc4e38cf81115b32e79a78ecebe86e3cf6869
root <alohanpraise2002@gmail.com>
2023-06-09 06:26:23 -0700

    muliply 2 digits

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
+ * main - function that Multiplies two given digits
+ * @argc: argument command line
+ * @argv: array of strings that stores the argument from command line
+ * Return: 0 if successful
  */
+int main(int argc, char *argv[])
+{
+	int numBer1, numBer2, temp;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	numBer1 = atoi(argv[1]);
+	numBer2 = atoi(argv[2]);
+
+	temp = numBer1 * numBer2;
+	printf("%d\n", temp);
+	return (0);
+}
