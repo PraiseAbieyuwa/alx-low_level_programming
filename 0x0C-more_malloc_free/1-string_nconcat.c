83099544994539846723e8d3bc45d5a523f008d6
root <alohanpraise2002@gmail.com>
2023-06-15 16:46:39 -0700

    concatenates two string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,21 +2,6 @@
 #include "main.h"
 
 /**
- * _strlen - calculate string length
- * @string:str
- * Return:length
- */
-
-int _strlen(char *string)
-{
-	int alt;
-
-	for (alt = 0; string[alt] != '\0'; alt++)
-		;
-	return (alt);
-}
-
-/**
  * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
  * @s1: string 1
  * @s2: string 2
@@ -27,30 +12,30 @@ int _strlen(char *string)
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	int temp, len, alt, sas;
-
-	temp = n;
+	unsigned int temp = 0, lent = 0, alt, sas;
 
 	if (s1 == NULL) /* account for NULL strings */
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (temp < 0) /* account for negative n bytes */
-		return (NULL);
-	if (temp >= _strlen(s2)) /* account for n too big */
-		temp = _strlen(s2);
+	while (s1[temp] != '\0')
+		temp++;
+	while (s2[lent] != '\0')
+		lent++;
+	if (n >= lent)
+	{
+		n = lent;
+	}
 
-	len = _strlen(s1) + temp + 1; /* +1 to account for null pointer */
-
-	ptr = malloc(sizeof(*ptr) * len); /* malloc and check for error */
+	ptr = (char *) malloc(sizeof(char) * (temp + n + 1));
 	if (ptr == NULL)
 		return (NULL);
 
-	for (alt = 0; s1[alt] != '\0'; alt++) /* concat */
+	for (alt = 0; alt < temp; alt++)
 		ptr[alt] = s1[alt];
-	for (sas = 0; sas < temp; sas++)
+	for (sas = 0; sas < n; sas++)
 		ptr[alt + sas] = s2[sas];
-	Ptr[alt + sas] = '\0';
+	Ptr[temp + n] = '\0';
 
 	return (ptr);
 }
