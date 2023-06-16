ab268da777161e4a7a49c8025f69d4b391dfd16f
root <alohanpraise2002@gmail.com>
2023-06-15 17:14:29 -0700

    creates array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 	size = max - min + 1;
-	arr = malloc(size * sizeof(int));
+	jax = malloc(size * sizeof(int));
 	if (jax == NULL)
 	{
 		return (NULL);
