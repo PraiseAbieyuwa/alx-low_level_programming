23a3fcee09acc94b3a946c21559e90455d64b374
root <alohanpraise2002@gmail.com>
2023-07-07 15:44:52 -0700

    clearthebits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - the function that set values to 0
+ * @n: element
+ * @index: starting from 0
+ * Return: return 1
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (inax >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	*n &= ~(1 << inax);
+	return (1);
+}
